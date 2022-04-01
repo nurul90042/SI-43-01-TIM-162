8223236eaac6264b1bb89d61d1e64af1f91cdcab
nurul90042 <wahdanurul.nw@gmail.com>
2022-04-01 22:57:15 +0700

    rev

--- rehab-in/database/migrations/2022_04_01_124429_create_regpasiens_table.php
+++ rehab-in/database/migrations/2022_04_01_124429_create_regpasiens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('regpasien', function (Blueprint $table) {
+        Schema::create('regpasiens', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('regpasien');
+        Schema::dropIfExists('regpasiens');
     }
 };
