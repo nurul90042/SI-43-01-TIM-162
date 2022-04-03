d013ae88282350a368177ef9bf651c62d37a9431
nurul90042 <wahdanurul.nw@gmail.com>
2022-04-04 05:23:24 +0700

    [Add: Background Landing Page ref #21]

--- rehab-in/resources/views/layouts/user.blade.php
+++ rehab-in/resources/views/layouts/user.blade.php
@@ -83,11 +83,12 @@ https://templatemo.com/tm-568-digimedia
             <!-- ***** Menu Start ***** -->
             <ul class="nav">
               <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-              <li class="scroll-to-section"><a href="#about">About</a></li>
               <li class="scroll-to-section"><a href="#services">Services</a></li>
-              <li class="scroll-to-section"><a href="#portfolio">Projects</a></li>
+              <li class="scroll-to-section"><a href="#about">About</a></li>
+              
+              {{-- <li class="scroll-to-section"><a href="#portfolio">Projects</a></li>
               <li class="scroll-to-section"><a href="#blog">Blog</a></li>
-              <li class="scroll-to-section"><a href="#contact">Contact</a></li> 
+              <li class="scroll-to-section"><a href="#contact">Contact</a></li>  --}}
               <li class="scroll-to-section"><div class="border-first-button"><a href="#contact">Free Quote</a></div></li> 
             </ul>        
             <a class='menu-trigger'>
